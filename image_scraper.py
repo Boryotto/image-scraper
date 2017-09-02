9c9ebae645b7362793c35998eb2f1b0bc59bc680
Mor Koshokaro <mor.k.747111@gmail.com>
2017-09-01 23:04:20 -0700

    fixed a bug that caused the webdriver to be ignored. fixed a bug with escaping spaces in given paths

--- image_scraper.py
+++ image_scraper.py
@@ -7,13 +7,14 @@ from argparse import ArgumentParser
 
 
 def main():
-    scrape_images(*parse_args())
+    args = parse_args()
+    scrape_images(*args)
 
 
 def parse_args():
     arg_parser = ArgumentParser(
         description="Download images from a website depending on an xpath of the img's father node")
-    arg_parser.add_argument('--parent-xpath', nargs=1, default="//body", type=str, required=False,
+    arg_parser.add_argument('--parent-xpath', nargs=1, default=r"//body", type=str, required=False,
                             help="An XPATH to the parent node of the images")
     arg_parser.add_argument('--url', nargs=1, type=str, required=True,
                             help="The URL of the webpage to scrape from")
@@ -21,21 +22,23 @@ def parse_args():
                             help="The destination folder absolute path")
     arg_parser.add_argument('--trim-url', action='store_true', required=False,
                             help="Trim image urls from query strings that might make the image minified")
-    arg_parser.add_argument('--webdriver-path', nargs=1, type=str, required=False, default="./chromedriver.exe",
+    arg_parser.add_argument('--webdriver-path', nargs=1, type=str, required=False, default=r"./chromedriver.exe",
                             help="An absolute path to a chrome Webdriver")
 
     namespace = arg_parser.parse_args()
-    parent_xpath = namespace.parent_xpath[0]
-    url = namespace.url[0]
-    destination_folder = namespace.dest[0]
+
+    parent_xpath = ''.join(namespace.parent_xpath)
+    url = ''.join(namespace.url)
+    destination_folder = ''.join(namespace.dest)
     trim_src_path = namespace.trim_url
+    webdriver = ''.join(namespace.webdriver_path)
     return url, parent_xpath, destination_folder, trim_src_path, webdriver
 
 
 def scrape_images(url, parent_xpath, destination_folder, trim_src_path, webdriver):
     with driver_starter(webdriver_path=webdriver) as driver:
         driver.get(url=url)
-        root_element = driver.find_elements_by_xpath('{}//img'.format(parent_xpath))
+        root_element = driver.find_elements_by_xpath(r'{}//img'.format(parent_xpath))
         image_srcs = [element.get_attribute('src') for element in root_element]
         for src in image_srcs:
             if trim_src_path:
